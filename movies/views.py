d70a22e31558ee5bec9c2953da80b195a2cbeecb
Sathish Veeranan <sathish.veeranan@iiitb.ac.in>
2022-05-30 13:06:37 +0530

    added changes

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Movie, Payment, PaymentIntent, Seat
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    movies=Movie.objects.all()
+    return render(request,"index.html",{
+        "movies":movies
+    })
