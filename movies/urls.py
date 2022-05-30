31d712fdd3bdc64c3e5286d6387701bd7a17c983
Sathish Veeranan <sathish.veeranan@iiitb.ac.in>
2022-05-30 14:13:36 +0530

    video 2 changes

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,occupiedSeats
 
 app_name = 'seat_booking'
 
 urlpatterns = [
-    path('',index,name="home")
+    path('',index,name="home"),
+    path('occupied/',occupiedSeats,name="occupied_seat"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .models import Movie, Payment, PaymentIntent, Seat
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+import json
 
 # Create your views here.
 def index(request):
     movies=Movie.objects.all()
     return render(request,"index.html",{
         "movies":movies
+    })
+
+@csrf_exempt
+def occupiedSeats(request):
+    data=json.loads(request.body)
+
+    movie=Movie.objects.get(title=data["movie_title"])
+    occupied=movie.booked_seats.all()
+    occupied_seat=list(map(lambda seat : seat.seat_no - 1,occupied))
+
+    return JsonResponse({
+        "occupied_seats":occupied_seat,
+        "movie":str(movie)
     })
