d95f5c0462d1ed966b05b375192b5856817860ca
Sathish Veeranan <sathish.veeranan@iiitb.ac.in>
2022-05-30 16:52:09 +0530

    added changes

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,occupiedSeats
+from .views import index,occupiedSeats,makePayement
 
 app_name = 'seat_booking'
 
 urlpatterns = [
     path('',index,name="home"),
     path('occupied/',occupiedSeats,name="occupied_seat"),
+    path('payment/',makePayement,name="payment")
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .models import Movie, Payment, PaymentIntent, Seat
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.conf import settings
 
 import json
+import requests;
 
 # Create your views here.
 def index(request):
@@ -23,4 +25,45 @@ def occupiedSeats(request):
     return JsonResponse({
         "occupied_seats":occupied_seat,
         "movie":str(movie)
+    })
+
+@csrf_exempt
+def makePayement(request):
+    data=json.loads(request.body)
+    seat_numbers=list(map(lambda seat: seat+1,data["seat_list"]))
+    movie_title=data["movie_title"]
+
+    cost=Movie.objects.get(title=movie_title).price
+
+    header={
+        "Authorization":f"Bearer {settings.PAYSTACK_SECRET}",
+        "Content-Type":"application/json"
+    }
+
+    data={
+        "name":"Payment of Movie Ticket",
+        "amount":int(cost*len(seat_numbers))*100,
+        "description":f"Payment for {len(seat_numbers)} ticket of {movie_title}",
+        "collect_phone":True,
+        "redirect_url":f"{settings.HOST_URL}/payment-confirm/"
+    }
+
+    response=requests.post('https://api.paystack.co/page',
+                            json=data,headers=header)
+
+    if response.status_code ==200:
+        response_data=response.json()
+        slug=response_data["data"]["slug"]
+        redirect_url=f"https://paystack.com/pay/{slug}"
+
+        PaymentIntent.objects.create(referrer=redirect_url,
+                                    movie_title=movie_title,
+                                    seat_number=seat_numbers)
+        
+        return JsonResponse({
+            "payment_url":redirect_url
+        })
+
+    return JsonResponse({
+        "error":"sorry service is not available"
     })
